e5ff676ed095327f09fd50883ad5b8fa5809a858
Minu Francis <mfrancis@5CG651017G-LA>
2018-10-26 16:39:46 -0500

    feature: added the funcitonality to start/stop wondows service via the module

--- index.js
+++ index.js
@@ -2,43 +2,44 @@ const exec = require("child_process").exec;
 
 exports.performIISOperations = (data, callback) => {
     if (data.action == 'start') {
-        exec("Powershell.exe  -executionpolicy remotesigned . " + __dirname + "\\actions.ps1; Start-IIS-Site -username " + data.username + " -password " + data.password + " -serverIpAddress " + data.serverIpAddress + " -siteName " + data.siteName , function(err, stdout, stderr){
-            console.log(stdout); 
+        exec("Powershell.exe  -executionpolicy remotesigned . " + __dirname + "\\actions.ps1; StartIISSite -username " + data.username + " -password " + data.password + " -serverIpAddress " + data.serverIpAddress + " -siteName " + data.name, function (err, stdout, stderr) {
+            console.log(stdout);
             callback(err)
-         });
+        });
     }
     else if (data.action == 'stop') {
-        exec("Powershell.exe  -executionpolicy remotesigned . " + __dirname + "\\actions.ps1; Stop-IIS-Site -username " + data.username + " -password " + data.password + " -serverIpAddress " + data.serverIpAddress + " -siteName " + data.siteName , function(err, stdout, stderr){
-            console.log(stdout); 
+        exec("Powershell.exe  -executionpolicy remotesigned . " + __dirname + "\\actions.ps1; StopIISSite -username " + data.username + " -password " + data.password + " -serverIpAddress " + data.serverIpAddress + " -siteName " + data.name, function (err, stdout, stderr) {
+            console.log(stdout);
             callback(err)
-         });
+        });
     }
 }
 
+exports.performWindowsServiceOperations = (data, callback) => {
+    if (data.action == 'start') {
+        exec("cmdkey.exe /add:" + data.serverIpAddress + " /user:" + data.username + " /pass:" + data.password, () => {
+            console.log('Starting service: ', data.name);
+            exec("PsExec64.exe -s \\\\" + data.serverIpAddress + " -u " + data.username + " -p " + data.password + " c:\\windows\\system32\\sc start " + data.name, () => {
+                console.log('Started service: ', data.name);
+                exec("cmdkey.exe /delete:" + data.serverIpAddress + " /user:" + data.username + " /pass:" + data.password, () => {
+                    console.log('Completed the requested operation.');
+                });
+            });
+        });
+    } else {
+        exec("cmdkey.exe /add:" + data.serverIpAddress + " /user:" + data.username + " /pass:" + data.password, () => {
+            console.log('Stopping service: ', data.name);
+            exec("PsExec64.exe -s \\\\" + data.serverIpAddress + " -u " + data.username + " -p " + data.password + " c:\\windows\\system32\\sc stop " + data.name, () => {
+                console.log('Stopped service: ', data.name);
+                exec("cmdkey.exe /delete:" + data.serverIpAddress + " /user:" + data.username + " /pass:" + data.password, () => {
+                    console.log('Completed the requested operation.');
+                });
+            });
+        });
+    }
 
-// performIISOperations(data, () => {
-//     console.log('Done with the action!!!!!');
-// });
+}
 
-// let startSite = () => {
-//     runGulpTask('startSite', 'gulpfile.js')
-//         .then(() => {
-//             console.log('Done with running the startSite gulp task.');
-//         })
-//         .catch((e) => {
-//             console.log('Error executing the gulp task - startSite');
-//         });
-// }
-
-// let stopSite = () => {
-//     runGulpTask('stopSite', 'index.js')
-//         .then(() => {
-//             console.log('Done with running the stopSite gulp task.');
-//         })
-//         .catch((e) => {
-//             console.log('Error executing the gulp task - stopSite');
-//         });
-// }
 
 
 
