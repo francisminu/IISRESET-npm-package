aff6a0d27ad090e7a997e957efcf07a179b2280a
mfrancis <minu.francis@geodis.com>
2018-10-29 16:54:23 -0500

    fix: Added null checks in the code

--- index.js
+++ index.js
@@ -1,45 +1,52 @@
 const exec = require("child_process").exec;
 
 exports.performIISOperations = (data, callback) => {
-    if (data.action == 'start') {
-        exec("Powershell.exe  -executionpolicy remotesigned . " + __dirname + "\\actions.ps1; StartIISSite -username " + data.username + " -password " + data.password + " -serverIpAddress " + data.serverIpAddress + " -siteName " + data.name, function (err, stdout, stderr) {
-            console.log(stdout);
-            callback(err)
-        });
-    }
-    else if (data.action == 'stop') {
-        exec("Powershell.exe  -executionpolicy remotesigned . " + __dirname + "\\actions.ps1; StopIISSite -username " + data.username + " -password " + data.password + " -serverIpAddress " + data.serverIpAddress + " -siteName " + data.name, function (err, stdout, stderr) {
-            console.log(stdout);
-            callback(err)
-        });
+    if (data) {
+        if (data.action == 'start') {
+            exec("Powershell.exe  -executionpolicy remotesigned . " + __dirname + "\\actions.ps1; StartIISSite -username " + data.username + " -password " + data.password + " -serverIpAddress " + data.serverIpAddress + " -siteName " + data.name, function (err, stdout, stderr) {
+                console.log(stdout);
+                callback(err)
+            });
+        }
+        else if (data.action == 'stop') {
+            exec("Powershell.exe  -executionpolicy remotesigned . " + __dirname + "\\actions.ps1; StopIISSite -username " + data.username + " -password " + data.password + " -serverIpAddress " + data.serverIpAddress + " -siteName " + data.name, function (err, stdout, stderr) {
+                console.log(stdout);
+                callback(err)
+            });
+        }
+    } else {
+        console.log('Data is null.');
     }
 }
 
 exports.performWindowsServiceOperations = (data, callback) => {
-    if (data.action == 'start') {
-        exec("cmdkey.exe /add:" + data.serverIpAddress + " /user:" + data.username + " /pass:" + data.password, () => {
-            console.log('Starting service: ', data.name);
-            exec(__dirname + "\\PsExec64.exe -s \\\\" + data.serverIpAddress + " -u " + data.username + " -p " + data.password + " c:\\windows\\system32\\sc start " + data.name, () => {
-                console.log('Started service: ', data.name);
-                exec("cmdkey.exe /delete:" + data.serverIpAddress + " /user:" + data.username + " /pass:" + data.password, () => {
-                    console.log('Completed the requested operation.');
-                    callback();
+    if (data) {
+        if (data.action == 'start') {
+            exec("cmdkey.exe /add:" + data.serverIpAddress + " /user:" + data.username + " /pass:" + data.password, () => {
+                console.log('Starting service: ', data.name);
+                exec(__dirname + "\\PsExec64.exe -s \\\\" + data.serverIpAddress + " -u " + data.username + " -p " + data.password + " c:\\windows\\system32\\sc start " + data.name, () => {
+                    console.log('Started service: ', data.name);
+                    exec("cmdkey.exe /delete:" + data.serverIpAddress + " /user:" + data.username + " /pass:" + data.password, () => {
+                        console.log('Completed the requested operation.');
+                        callback();
+                    });
                 });
             });
-        });
+        } else {
+            exec("cmdkey.exe /add:" + data.serverIpAddress + " /user:" + data.username + " /pass:" + data.password, () => {
+                console.log('Stopping service: ', data.name);
+                exec(__dirname + "\\PsExec64.exe -s \\\\" + data.serverIpAddress + " -u " + data.username + " -p " + data.password + " c:\\windows\\system32\\sc stop " + data.name, () => {
+                    console.log('Stopped service: ', data.name);
+                    exec("cmdkey.exe /delete:" + data.serverIpAddress + " /user:" + data.username + " /pass:" + data.password, () => {
+                        console.log('Completed the requested operation.');
+                        callback();
+                    });
+                });
+            });
+        }
     } else {
-        exec("cmdkey.exe /add:" + data.serverIpAddress + " /user:" + data.username + " /pass:" + data.password, () => {
-            console.log('Stopping service: ', data.name);
-            exec(__dirname + "\\PsExec64.exe -s \\\\" + data.serverIpAddress + " -u " + data.username + " -p " + data.password + " c:\\windows\\system32\\sc stop " + data.name, () => {
-                console.log('Stopped service: ', data.name);
-                exec("cmdkey.exe /delete:" + data.serverIpAddress + " /user:" + data.username + " /pass:" + data.password, () => {
-                    console.log('Completed the requested operation.');
-                    callback();
-                });
-            });
-        });
+        console.log('Data is null.');
     }
-
 }
 
 
